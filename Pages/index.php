adfb9b0147328c182bea597504d7d5055b3657c6
Feeew <fewful@hotmail.fr>
2014-03-18 14:33:57 +0100

    V1.1.1
    Correction du titre h1 de la page Historique des tournois
    Correction du message d'erreur de Index pour la connexion
    Suppression du test de session pour la page d'inscription

--- Pages/index.php
+++ Pages/index.php
@@ -31,7 +31,7 @@ else{
 if(isset($_GET["err"])){
 	$erreur = $_GET["err"];
 	switch($erreur){
-		case "1": echo "<h4 style='color:red;'>Cet utilisateur n'existe pas.</h4>";break;
+		case "1": echo "<h4 style='color:red;'>Erreur de connexion : L'utilisateur n'existe pas ou le mot de passe est erronné.</h4>";break;
 		case "2": echo "<h4 style='color:red;'>Merci de renseigner tous les champs.</h4>";break;
 		case "3": echo "<h4 style='color:red;'>Une erreur s'est produite, merci de contacter l'administrateur.</h4>";break;
 		case "4": echo "<h4 style='color:red;'>Votre session a expir&eacute;, merci de vous reconnecter.</h4>";break;
