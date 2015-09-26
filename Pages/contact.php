2f9d8852f706aac68fa988cd4062d01def87c2ae
Benjamin Seillier <fewful@hotmail.fr>
2015-09-26 13:00:05 +0200

    Ajout tableau suggestion
    La page des suggestions affichent maintenant toutes les suggestions pour
    les administrateurs
    Les pages de contacts s'ouvrent dans un nouvel onglet

--- Pages/contact.php
+++ Pages/contact.php
@@ -29,23 +29,23 @@ include 'header.php';
 	</tr>
 	<tr>
 		<td class="contact_lib">Github</td>
-		<td class="contact_value"><a href="https://github.com/Feeew">https://github.com/Feeew</a></td>
+		<td class="contact_value"><a href="https://github.com/Feeew" target="_blank">https://github.com/Feeew</a></td>
 	</tr>
 	<tr>
 		<td class="contact_lib">Tweeter</td>
-		<td class="contact_value"><a href="https://twitter.com/Fewful">https://twitter.com/Fewful</a></td>
+		<td class="contact_value"><a href="https://twitter.com/Feeew_" target="_blank">https://twitter.com/Feeew_</a></td>
 	</tr>
 	<tr>
 		<td class="contact_lib">Facebook</td>
-		<td class="contact_value"><a href="https://www.facebook.com/benjamin.seillier">https://www.facebook.com/benjamin.seillier</a></td>
+		<td class="contact_value"><a href="https://www.facebook.com/benjamin.seillier" target="_blank">https://www.facebook.com/benjamin.seillier</a></td>
 	</tr>
 	<tr>
 		<td class="contact_lib">Google +</td>
-		<td class="contact_value"><a href="https://plus.google.com/u/0/100719431492237541953/posts">https://plus.google.com/u/0/100719431492237541953/posts</a></td>
+		<td class="contact_value"><a href="https://plus.google.com/u/0/100719431492237541953/posts" target="_blank">https://plus.google.com/u/0/100719431492237541953/posts</a></td>
 	</tr>
 	<tr>
 		<td class="contact_lib">Youtube</td>
-		<td class="contact_value"><a href="https://www.youtube.com/channel/UCihkGqiYixtfZCt4caxgStg">https://www.youtube.com/channel/UCihkGqiYixtfZCt4caxgStg</a></td>
+		<td class="contact_value"><a href="https://www.youtube.com/channel/UCihkGqiYixtfZCt4caxgStg" target="_blank">https://www.youtube.com/channel/UCihkGqiYixtfZCt4caxgStg</a></td>
 	</tr>
 
 </div>
