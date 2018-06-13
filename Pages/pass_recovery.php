ebc0262dada6deeae40626f52c6b663e28dba36c
akogare62 <jacqueminclement@hotmail.fr>
2018-06-13 19:14:27 +0200

    modification pour code plus propre, fonction smtpmailler mise a part

--- Pages/pass_recovery.php
+++ Pages/pass_recovery.php
@@ -8,71 +8,21 @@ require_once('../Scripts/PHPMailer-master/PHPMailerAutoload.php');
 
 include 'header.php';
 
+include '../Scripts/smtpmailer.php';
 
-
-function smtpmailer($to, $from, $from_name, $subject, $body) { 
-
-  global $error;
-
-  $mail = new PHPMailer();  // create a new object
-
-  $mail->IsSMTP(); // enable SMTP
-
-  $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
-
-  $mail->SMTPAuth = true;  // authentication enabled
-
-  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
-
-  $mail->Host = 'smtp.gmail.com';
-
-  $mail->Port = 465; 
-
-  $mail->Username = GUSER;  
-
-  $mail->Password = GPWD;           
-
-  $mail->SetFrom($from, $from_name);
-
-  $mail->Subject = $subject;
-
-  $mail->Body = $body;
-
-  $mail->IsHTML(true);  
-
-  $mail->AddAddress($to);
-
-  if(!$mail->Send()) {
-
-    $error = 'Mail error: '.$mail->ErrorInfo; 
-    return false;
-
-  } else {
-
-    $error = 'Message sent!';
-
-    return true;
-
-  }
-
-}
-
-$sql = "SELECT * FROM Users";
 if(count($_POST)){
+  $sql = "SELECT * FROM Users WHERE Email = '".$_POST['email']."' ";
 	foreach ($db->query($sql) as $row) {
 		if($_POST['email'] == $row['Email']){
 			$token = md5(time() . mt_rand());		
 			$message = 'Vous avez demande a redefinir votre mot de passe , Merci de cliquer sur le lien ci dessous.
 			http://shyrel.byethost6.com/Pages/newpass.php?e='.urlencode ($_POST["email"]).'&t='.urlencode ($token).'';
       smtpmailer($row['Email'], 'benji.seillier@gmail.com', 'Benjamin SEILLIER', 'Site de pronostique', $message);
-      //db update token here
       $sql1 = "UPDATE `Users` SET  Token = '".$token."' WHERE Email = '".$_POST['email']."' ";
       $db->exec($sql1);
       echo '<div class="alert alert-success"><strong>Success!</strong> Une email vous a &eacute;t&eacute; envoy&eacute; ! </div>';
 		}
-	}
-	
-	 
+	}	 
 }
 
 
---
+++ Scripts/global_dev.php
@@ -0,0 +1,24 @@
+<?php
+
+// Afficher les erreurs &agrave; l'&eacute;cran
+ini_set('display_errors', 1);
+// Enregistrer les erreurs dans un fichier de log
+ini_set('log_errors', 1);
+// Nom du fichier qui enregistre les logs (attention aux droits &agrave; l'&eacute;criture)
+ini_set('error_log', dirname(__file__) . '/logs/log_error_php.txt');
+
+//PARAMETRES ET BDD
+try{
+    $dsn = 'mysql:dbname=Rugby2014;host=localhost';
+	$user = 'root';
+	$password = '';
+	try {
+		$db = new PDO($dsn, $user, $password);
+	} catch (PDOException $e) {
+		echo "connexion echouee : " . $e->getMessage();
+	}
+} catch(Exception $e) {
+    echo "Impossible d'acc&eacute;der &agrave; la base de donn&eacute;es SQL : ".$e->getMessage();
+    die();
+}
+?>
