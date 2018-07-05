532f8ecde3e3f4d4e54d14c8a8d44fa86874bdfc
akogare62 <jacqueminclement@hotmail.fr>
2018-07-05 21:21:19 +0200

    sticky header / modification des commentaires
    sticky header / modification des commentaires

--- Pages/creation_tournoi.php
+++ Pages/creation_tournoi.php
@@ -84,7 +84,55 @@ else
 
 			require_once('../Scripts/PHPMailer-master/PHPMailerAutoload.php');
 
-			include '../Scripts/smtpmailer.php';
+
+
+			function smtpmailer($to, $from, $from_name, $subject, $body) { 
+
+				global $error;
+
+				$mail = new PHPMailer();  // create a new object
+
+				$mail->IsSMTP(); // enable SMTP
+
+				$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+
+				$mail->SMTPAuth = true;  // authentication enabled
+
+				$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+
+				$mail->Host = 'smtp.gmail.com';
+
+				$mail->Port = 465; 
+
+				$mail->Username = GUSER;  
+
+				$mail->Password = GPWD;           
+
+				$mail->SetFrom($from, $from_name);
+
+				$mail->Subject = $subject;
+
+				$mail->Body = $body;
+
+				$mail->IsHTML(true);  
+
+				$mail->AddAddress($to);
+
+				if(!$mail->Send()) {
+
+					$error = 'Mail error: '.$mail->ErrorInfo; 
+
+					return false;
+
+				} else {
+
+					$error = 'Message sent!';
+
+					return true;
+
+				}
+
+			}
 
 			$message .= "Bonjour, <br /> Un nouveau tournoi vient de commencer <br /> Titre : ".$tournoi_nom." <br /> Pour vous inscrire rendez vous &agrave; cette URL <br /> http://shyrel.byethost6.com/Pages/inscription_tournoi.php";
 
