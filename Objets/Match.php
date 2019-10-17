a929ec7c2c871c38ada3c4f6f419722aa51e4a67
Feeew <fewful@hotmail.fr>
2019-10-17 13:57:47 +0200

    Create Match.php

---
+++ Objets/Match.php
@@ -0,0 +1,8 @@
+class Match {
+    // constructor
+    public function __construct($id, $equipe1, $equipe2) {
+        $this->id = $id;
+        $this->equipe1 = $equipe1;
+        $this->equipe2 = $equipe2;
+    }
+}
